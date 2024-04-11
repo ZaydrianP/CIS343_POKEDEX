b33cf77c07c1b065712f23bffb6002f0669b00a4
ALostGuardian <98481015+ALostGuardian@users.noreply.github.com>
2024-04-11 09:19:59 -0400

    be the very best...

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,23 @@
+/*****************************************************************
+
+Pokedex api project for 343
+
+@author Isabella Snyder, Zaydrian Price
+
+@version Winter 2024
+
+*****************************************************************/
+//variables
+
+const base_url = 'https://pokeapi.co/api/v2/';
+
+
+/*****************************************************************
+
+Base stuff for the program
+
+*****************************************************************/
+
 const fetch = require('node-fetch');
 
 const readline = require('readline').createInterface({
@@ -5,51 +25,155 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 
-fetch('https://pokeapi.co/api/v2')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-    run();
-function showMenu() {
-    console.log("Enter the following to search for:\n1. Pokemon\n2. Item\n3. Move\n4. Exit")
-    .then(search_result => {
-        if (search_result == "Pokemon"){
+//fetching the data from the api using the base_url and then appending based on the search term
+fetch(base_url)
 
-        }
-        else if(search_result == "Item"){
+//turning response into json as that will be easier to work with
+    .then(response => {
+        const response_holder = response.json();
 
-        }
-        else if(search_result == "Move"){
-
-        }
-        else if(search_result == "Exit"){
-
-        }
-        else {
-            console.log("Wrong search term. Please try again.");
-            run();
-        }
+        //returning the json data
+        return response_holder;
     })
-    .then(prompt(search_result));
+    //pulling the results from the json data
+    .then(data => {
+        const query_result = data.results;
+    })
+    
+
+
+
+/*****************************************************************
+
+Menu functions
+
+*****************************************************************/
+
+//showMenu function is to display the main menu to the user
+function showMenu() {
+    //showing the menu options
+    console.log("Welcome to the Pokedex!");
+
+    //option 1, pokemon search
+    console.log("1. Search for a Pokemon");
+
+    //option 2, item search
+    console.log("2. Search for an Item");
+
+    //option 3, move search
+    console.log("3. Search for a Move");
+
+    //option 4, exit
+    console.log("4. Exit");
+
+    //prompting the user for input
+    readline.question("Please select an option: ", (option) => {
+        //switch statement to determine which option the user selected
+        switch (option) {
+            case '1':
+                prompt(searchPoke);
+                break;
+            case '2':
+                prompt(searchItem);
+                break;
+            case '3':
+                prompt(searchMove);
+                break;
+            case '4':
+                console.log("Goodbye!");
+                readline.close();
+                break;
+            default:
+                console.log("Invalid input, please try again.");
+                showMenu();
+        }
+    });
 }
 
+//prompt function is to prompt the user for input, handling readline functionality
 function prompt(cb) {
    
 }
 
-//searchPoke(term)
+//searching for a pokemon
+function searchPoke() {
+    readline.question("Enter the name of the pokemon you would like to search for: ", (term) => {
+        fetch(base_url + 'pokemon/' + term)
+            .then(response => {
+                const response_holder = response.json();
+                return response_holder;
+            })
+            .then(data => {
+                const query_result = data.results;
+            })
+    })
+}
 
-//printPoke(json)
+//printing the pokemon data
+function printPoke(json) {
+    console.log("Name: " + json.name);
+    console.log("Height: " + json.height);
+    console.log("Weight: " + json.weight);
+    console.log("Types: ");
+    for (let i = 0; i < json.types.length; i++) {
+        console.log(json.types[i].type.name);
+    }
+    console.log("Abilities: ");
+    for (let i = 0; i < json.abilities.length; i++) {
+        console.log(json.abilities[i].ability.name);
+    }
+}
 
-//searchItem(term)
+//searching for an item
+function searchItem() {
+    readline.question("Enter the name of the item you would like to search for: ", (term) => {
+        fetch(base_url + 'item/' + term)
+            .then(response => {
+                const response_holder = response.json();
+                return response_holder;
+            })
+            .then(data => {
+                const query_result = data.results;
+            })
+    })
+}
 
-//printItem(json)
+//print item 
+function printItem(json) {
+    console.log("Name: " + json.name);
+    console.log("Category: " + json.category.name);
+    console.log("Cost: " + json.cost);
+    console.log("Effect: " + json.effect_entries[0].effect);
+}
 
-//searchMove(term)
+//searching for a move
+function searchMove() {
+    readline.question("Enter the name of the move you would like to search for: ", (term) => {
+        fetch(base_url + 'move/' + term)
+            .then(response => {
+                const response_holder = response.json();
+                return response_holder;
+            })
+            .then(data => {
+                const query_result = data.results;
+            })
+    })
+}
 
-//printMove(json)
+//printing the move data
+function printMove(json) {
+    console.log("Name: " + json.name);
+    console.log("Type: " + json.type.name);
+    console.log("Power: " + json.power);
+    console.log("PP: " + json.pp);
+    console.log("Accuracy: " + json.accuracy);
+    console.log("Effect: " + json.effect_entries[0].effect);
+}
 
+
+
+//run functions is to start the program by running the main menu function
 function run() {
     showMenu();
-}
+};
 
