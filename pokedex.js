ac9f6adfc5ceb3f734e952de307df1e1e5c840f5
Izzy2002 <snyderis@mail.gvsu.edu>
2024-04-11 19:33:26 -0400

    to catch them is my real test *.*

--- pokedex.js
+++ pokedex.js
@@ -12,32 +12,6 @@ Pokedex api project for 343
 const base_url = 'https://pokeapi.co/api/v2/';
 
 
-/*****************************************************************
-
-Base stuff for the program
-
-*****************************************************************/
-
-const fetch = require('node-fetch');
-
-
-
-//fetching the data from the api using the base_url and then appending based on the search term
-fetch(base_url)
-
-//turning response into json as that will be easier to work with
-    .then(response => {
-        const response_holder = response.json();
-
-        //returning the json data
-        return response_holder;
-    })
-    //pulling the results from the json data
-    .then(data => {
-        const query_result = data.results;
-    })
-    
-
 
 
 /*****************************************************************
@@ -99,18 +73,11 @@ function showMenu() {
     });
 };
 
-//prompt function is to prompt the user for input, handling readline functionality
-function prompt(cb) {
+/*****************************************************************
 
-// Load the readline library
-const readline = require("readline");
-// Setup readline to listen on the stdin stream
-const rl = readline.createInterface(process.stdin, process.stdout);
-    readline.question("Please input an option: ", (input) => {
-        //callback function to handle the input
-        cb(input);
-    });
-}
+Pokemon functions
+
+*****************************************************************/
 
 //searching for a pokemon
 function searchPoke(term) {
@@ -119,38 +86,54 @@ function searchPoke(term) {
         if (term === "") {
             console.log("Invalid input, please try again.");
             showMenu();
+            return;
         }
 
         //fetch all pokemon and store in a list to verify that term is valid
         fetch(base_url + 'pokemon/')
-            .then(response_allpoke => {
-                const all_pokemon = response_allpoke.json();
-            })
-        
-        //check if the term is valid
-        if(all_pokemon.includes(term) === false) {
-            console.log("Invalid input, please try again.");
-            showMenu();
-        }
-        
-        //if gotten here then its a valid term
-        fetch(base_url + 'pokemon/' + term)
-            .then(response => {
-                const response_holder = response.json();
-                return response_holder;
-            })
-            .then(data => {
-                const query_result = data.results;
+        //after looking at the documentation and project examples they provide on their site I believe that we need to append things from this list
+        //it looks like this will return all of the pokemon but they will have name and url, im going to try and cut url out so that were not wasting extra time checking if our name is equal to the url
+
+            //okay so we first use a then to get the list and then we use a then so that once we have the data we can put it into a pokemon list consts
+            .then(response => response.json())
+
+            //this is where we are going to put the data into the pokemon list
+            .then(pokemon_list => {
+                //we have the data so now we are going to put it into the pokemon list
+                const pokemon = pokemon_list.results.map(pokemon => pokemon.name);
+
+                //now we can check if its a valid name!
+                if (!pokemon_list.includes(term)) {
+                    console.log("Invalid input, please try again.");
+                    showMenu();
+                    
+                } 
+                //if we hit this else that means they have a valid name and we can now fetch the data
+                else {
+                    //fetch the pokemon data
+                    fetch(base_url + 'pokemon/' + term)
+                        .then(response => response.json())
+                        .then(poke_data => {
+                            //we have it so now we can use our print function to display it
+                            printPoke(poke_data);
+                        })
+                        //trying to catch any errors, not sure if it works here I will have to test it
+                        .catch(error => {
+                            console.log("Error: " + error);
+                            showMenu();
+                        });
+                }
             });
-        
-        //print the pokemon data
-        printPoke(query_result);
-    
+      
+       
     
 };
 
     //printing the pokemon data
     function printPoke(json) {
+    // after lookinging at the docs I think these are the variables we can get about the mons so imma orint them to the console
+
+    //printing the name, height, weight, types, and abilities
     console.log("Name: " + json.name);
     console.log("Height: " + json.height);
     console.log("Weight: " + json.weight);
@@ -164,21 +147,59 @@ function searchPoke(term) {
     }
 }
 
-//searching for an item
-function searchItem() {
-    readline.question("Enter the name of the item you would like to search for: ", (term) => {
-        fetch(base_url + 'item/' + term)
-            .then(response => {
-                const response_holder = response.json();
-                return response_holder;
-            })
-            .then(data => {
-                const query_result = data.results;
-            })
-    })
+/*****************************************************************
+
+Item functions
+
+*****************************************************************/
+
+//searching for an item, very similar to search poke?? so Ill copy the code from there and try and just adjust it to work for items
+function searchItem(term) {
+     //check if the term is empty
+     if (term === "") {
+        console.log("Invalid input, please try again.");
+        showMenu();
+        return;
+    }
+    //fetching all the items, ill look and see how theyre formatted later and fix formating if needed
+    //fetch item
+    fetch(base_url + 'item/')
+    
+
+        //for this im not going to map anything ill just search the whole thing for now
+        .then(response => response.json())
+
+        //putting the data of all items in a list
+        .then(item_list => {
+            
+            //ill try mapping item name for now ill see if that works
+            const items = item_list.results.map(item => item.name);
+
+            //checking its valid
+            if (!items.includes(term)) {
+                console.log("Invalid input, please try again.");
+                showMenu();
+                
+            } 
+            //if we hit this else that means they have a valid name and we can now fetch the data
+            else {
+                //fetch the pokemon data
+                fetch(base_url + 'item/' + term)
+                    .then(response => response.json())
+                    .then(item_data => {
+                        //we have it so now we can use our print function to display it
+                        printItem(item_data);
+                    })
+                    //trying to catch any errors, not sure if it works here I will have to test it
+                    .catch(error => {
+                        console.log("Error: " + error);
+                        showMenu();
+                    });
+            }
+        });
 }
 
-//print item 
+//print item  data
 function printItem(json) {
     console.log("Name: " + json.name);
     console.log("Category: " + json.category.name);
@@ -186,18 +207,55 @@ function printItem(json) {
     console.log("Effect: " + json.effect_entries[0].effect);
 }
 
-//searching for a move
+/*****************************************************************
+
+Move's functions
+
+*****************************************************************/
+
+
+//coping over code again and adjusting it to work for moves
 function searchMove() {
-    readline.question("Enter the name of the move you would like to search for: ", (term) => {
-        fetch(base_url + 'move/' + term)
-            .then(response => {
-                const response_holder = response.json();
-                return response_holder;
-            })
-            .then(data => {
-                const query_result = data.results;
-            })
-    })
+    //check if the term is empty
+    if (term === "") {
+        console.log("Invalid input, please try again.");
+        showMenu();
+        return;
+    }
+    //fetch all the moves
+    fetch(base_url + 'moves/')
+    ``
+        //for this im not going to map anything ill just search the whole thing for now
+        .then(response => response.json())
+
+        //putting the data of all items in a list
+        .then(move_list => {
+            
+            //ill try mapping item name for now ill see if that works
+            const moves = move_list.results.map(move => move.name);
+
+            //checking its valid
+            if (!moves.includes(term)) {
+                console.log("Invalid input, please try again.");
+                showMenu();
+                
+            }
+            //if we hit this else that means they have a valid name and we can now fetch the data
+            else {
+                //fetch the pokemon data
+                fetch(base_url + 'move/' + term)
+                    .then(response => response.json())
+                    .then(move_data => {
+                        //we have it so now we can use our print function to display it
+                        printMove(move_data);
+                    })
+                    //trying to catch any errors, not sure if it works here I will have to test it
+                    .catch(error => {
+                        console.log("Error: " + error);
+                        showMenu();
+                    });
+            }
+        });
 }
 
 //printing the move data
@@ -211,9 +269,33 @@ function printMove(json) {
 }
 
 
+/*****************************************************************
+
+helper functions
+
+*****************************************************************/
+//readline stuff
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 //run functions is to start the program by running the main menu function
 function run() {
     showMenu();
 };
 
+
+//prompt function is to prompt the user for input, handling readline functionality
+function prompt(cb) {
+
+    // Load the readline library
+    const readline = require("readline");
+    // Setup readline to listen on the stdin stream
+    const rl = readline.createInterface(process.stdin, process.stdout);
+        readline.question("Please input an option: ", (input) => {
+            //callback function to handle the input
+            cb(input);
+        });
+    }
